5df7a4ac066bdfb152c83be50cc64d74933858c4
Ryszard Plewnia <hello@erys.io>
2022-09-26 11:47:09 +0200

    Add Rating component

---
+++ src/components/Rating/Rating.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+import Star from "../../icons/Star";
+
+interface RatingProps {
+  rating: number | null;
+  onChange?: (rate: number | null) => void;
+}
+
+const Rating: React.FC<RatingProps> = ({ rating, onChange }) => {
+  const [hovered, setHovered] = useState<number | null>(null);
+
+  return (
+    <div className="flex flex-row gap-0.5">
+      {Array(5)
+        .fill(null)
+        .map((_, i) => (
+          <Star
+            key={i}
+            onMouseEnter={() => onChange && setHovered(i)}
+            onMouseLeave={() => onChange && setHovered(null)}
+            className={onChange && "cursor-pointer"}
+            onClick={() =>
+              onChange && onChange(rating !== i + 1 ? i + 1 : null)
+            }
+            starProps={{
+              className:
+                hovered !== null && i <= hovered && rating && i < rating
+                  ? "fill-yellow-400"
+                  : hovered !== null && i <= hovered
+                  ? "fill-yellow-200"
+                  : rating && i < rating
+                  ? "fill-yellow-300"
+                  : undefined,
+            }}
+          />
+        ))}
+    </div>
+  );
+};
+
+export default Rating;
