03e03bfdfdfc6010b1d845d801650607117f818c
Ryszard Plewnia <hello@erys.io>
2022-10-05 15:15:47 +0200

    Comment test file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders Hello world text", () => {
+// TODO: mock API responses and test other functionalities
+
+test("renders all hotels in list", async () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hello world!/i);
-  expect(linkElement).toBeInTheDocument();
+  const hotel1 = await screen.findByText("OBM Hotel 1");
+  const hotel2 = await screen.findByText("OBM Hotel 2");
+  const hotel3 = await screen.findByText("OBM Hotel 3");
+  const hotel4 = await screen.findByText("OBM Hotel 4");
+  expect(hotel1).toBeInTheDocument();
+  expect(hotel2).toBeInTheDocument();
+  expect(hotel3).toBeInTheDocument();
+  expect(hotel4).toBeInTheDocument();
 });
